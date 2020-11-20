1ddd4506fc5ad10c04be0af14f4c6479fd4240f4
Antonio_Leon <antonio@glouu.com>
2020-11-20 22:40:07 +0000

    layoutsPO

---
+++ webapp/controller/invoiceUpload/DetailInvoiceUpload.controller.js
@@ -0,0 +1,16 @@
+// @ts-ignore
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("ns.EBilliaApp.controller.DetailInvoiceUpload", {
+
+            onInit: function () {
+console.log('on Detail PO component view')
+            },
+           
+           
+        });
+    })
---
+++ webapp/controller/invoiceUpload/MasterInvoiceUpload.controller.js
@@ -0,0 +1,16 @@
+// @ts-ignore
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("ns.EBilliaApp.controller.MasterInvoiceUpload", {
+
+            onInit: function () {
+console.log('on Master PO component view')
+            },
+           
+           
+        });
+    })
